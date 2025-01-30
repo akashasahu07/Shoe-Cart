ed8a5cb39125885ef296631deba9cd414daba381
Akasha Sahu <akashasahu2001@gmail.com>
2025-01-30 14:26:48 +0530

    Update main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,38 +9,57 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-  searchText:string = ''
-  text:string = ''
+  searchText: string = ''
+  text: string = ''
   count = 0
 
+  // All Products List Here
   products = [
     {
       image: "https://cdn.media.amplience.net/i/scvl/144406_308584_1?fmt=auto&w=640",
       name: "Men's New Balance 515 Sustainable Sneakers'",
       price: "6,200",
-      discount:  "8,999"
+      discount: "8,999",
+      inStock: true
     },
 
     {
       image: "https://cdn.media.amplience.net/i/scvl/151851_341697_1?fmt=auto&w=640",
       name: "Women's New Balance ML408 Running Shoes",
       price: "4,099",
-      discount:  "4,999"
+      discount: "4,999",
+      inStock: false
     },
 
     {
       image: "https://cdn.media.amplience.net/s/scvl/144779_359040_SET/1?fmt=auto&$webPdpProduct$",
       name: "Girls' New Balance Big Kid 515 Running Shoes",
       price: "6,099",
-      discount:  "6,999"
+      discount: "6,999",
+      inStock: true
     }
   ]
 
+  // This is for the search button
   searchButton() {
     this.text = this.searchText
   }
 
+  // This is for adding product to the Cart
+  ButtonClick(product: any) {
+    if (!product.inStock) {
+      alert("Product is out of stock. You will be notified when it's Available.");
+    }
+    else {
+      this.addToCart();
+    }
+  }
+
   addToCart() {
-    this.count ++;
+    const audio = new Audio('click.mp3') //This is for playing audio
+    audio.play()
+
+    this.count++;
+
   }
 }
