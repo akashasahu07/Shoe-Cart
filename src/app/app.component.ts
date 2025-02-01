cbdd7ae4169d5d3b848ad2c63dc85b7d68b3b1b1
Akasha Sahu <akashasahu2001@gmail.com>
2025-02-01 23:33:46 +0530

    Footer Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { CartComponent } from './cart/cart.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MainComponent, CartComponent],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, CartComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
